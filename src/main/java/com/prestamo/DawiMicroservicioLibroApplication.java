094415bd440bea3ea1f500534a64fef998d498b8
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-10-12 18:27:03 -0500

    ajustes

--- src/main/java/com/prestamo/DawiMicroservicioLibroApplication.java
+++ src/main/java/com/prestamo/DawiMicroservicioLibroApplication.java
@@ -2,6 +2,8 @@ package com.prestamo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DawiMicroservicioLibroApplication {
@@ -10,4 +12,8 @@ public class DawiMicroservicioLibroApplication {
 		SpringApplication.run(DawiMicroservicioLibroApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/prestamo/controller/LibroController.java
+++ src/main/java/com/prestamo/controller/LibroController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.prestamo.entity.Libro;
 import com.prestamo.service.LibroService;
@@ -16,11 +17,15 @@ import com.prestamo.service.LibroService;
 public class LibroController {
 
 	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
 	private LibroService libroService;
 	
 	
 	@GetMapping("/listar")
 	public List<Libro> listar() {
+		restTemplate.postForObject("http://localhost:8091/url/auditoria/registrar?tipoBaseDatos=MONGODB&baseDeDatos=Cibertec&tabla=Libro", null, String.class);
 		return libroService.findAll();
 	}
 	
